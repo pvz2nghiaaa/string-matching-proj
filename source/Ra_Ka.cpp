e55bdc25cf0a159e92bac8cf1d10ff9aa302f09e
HwngMP <pmai4118@gmail.com>
2026-03-28 13:11:18 +0700

    rabin-karp algrithm not done operation count

--- source/Ra_Ka.cpp
+++ source/Ra_Ka.cpp
@@ -9,7 +9,7 @@ ll comp_hash(const string &str, int m)
     ll h = 0;
     for (int i = 0; i < m; i++)
     {
-        h += (h * BASE + (str[i] - 'A' + 1)) % MOD;
+        h += (h * BASE + (str[i] - 'a' + 1)) % MOD;
     }
     return h;
 }
@@ -23,9 +23,134 @@ vector<vector<pair<pair<int, int>, pair<int, int>>>> Rabin_karp(vector<vector<ch
 
     for (const auto &word : keywords)
     {
+        vector<pair<pair<int, int>, pair<int, int>>> res;
+        int m = word.length();
+        if (m == 0 || m > max(rows, cols))
+        {
+            all_res.push_back(res);
+            continue;
+        }
+
+        ll HashWord = comp_hash(word, m);
+
+        ll h_inv = 1;
+        for (int i = 0; i < m; i++)
+            h_inv = (h_inv * BASE) % MOD;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                for (int gir = 0; gir < 8; gir++)
+                {
+                    int endRow = r + (m - 1) * dx[gir];
+                    int endCol = c + (m - 1) * dy[gir];
+
+                    if (endRow > 0 && endRow < rows && endCol > 0 && endCol < cols)
+                    {
+                        ll h_cur = 0;
+                        bool match = 1;
+
+                        for (int i = 0; i < m; i++)
+                        {
+                            h_cur += (h_cur * BASE + grids[r + i + dx[gir]][c + i + dy[gir]] - 'a' + 1) % MOD;
+                        }
+
+                        if (h_cur == HashWord)
+                        {
+                            for (int i = 0; i < m; i++)
+                            {
+                                if (grids[r + i + dx[gir]][c + i + dy[gir]] != word[i])
+                                {
+                                    match = 0;
+                                    break;
+                                }
+                            }
+                            if (match)
+                                res.push_back({{r, c}, {endRow, endCol}});
+                        }
+                    }
+                }
+            }
+        }
+        all_res.push_back(res);
     }
+
+    return all_res;
 }
 
-vector<vector<pair<pair<int, int>, pair<int, int>>>> Rabin_karpCountOperation(vector<vector<char>> &grids, vector<string> &keywords, int &comparisons)
+ll comp_hash_OC(const string &str, int m)
 {
+    ll h = 0;
+    for (int i = 0; i < m; i++)
+    {
+        h += (h * BASE + (str[i] - 'a' + 1)) % MOD;
+    }
+    return h;
+}
+
+vector<vector<pair<pair<int, int>, pair<int, int>>>> Rabin_karpOperationCount(vector<vector<char>> &grids, vector<string> &keywords, int &comparisons)
+{
+    int rows = grids.size();
+    int cols = (rows > 0) ? grids[0].size() : 0;
+
+    vector<vector<pair<pair<int, int>, pair<int, int>>>> all_res;
+
+    for (const auto &word : keywords)
+    {
+        ++comparisons;
+        vector<pair<pair<int, int>, pair<int, int>>> res;
+        int m = word.length();
+        if (++comparisons, m == 0 || m > max(rows, cols))
+        {
+            all_res.push_back(res);
+            continue;
+        }
+
+        ll HashWord = comp_hash(word, m);
+
+        ll h_inv = 1;
+        for (int i = 0; i < m; i++)
+            h_inv = (h_inv * BASE) % MOD;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                for (int gir = 0; gir < 8; gir++)
+                {
+                    int endRow = r + (m - 1) * dx[gir];
+                    int endCol = c + (m - 1) * dy[gir];
+
+                    if (endRow > 0 && endRow < rows && endCol > 0 && endCol < cols)
+                    {
+                        ll h_cur = 0;
+                        bool match = 1;
+
+                        for (int i = 0; i < m; i++)
+                        {
+                            h_cur += (h_cur * BASE + grids[r + i + dx[gir]][c + i + dy[gir]] - 'a' + 1) % MOD;
+                        }
+
+                        if (h_cur == HashWord)
+                        {
+                            for (int i = 0; i < m; i++)
+                            {
+                                if (grids[r + i + dx[gir]][c + i + dy[gir]] != word[i])
+                                {
+                                    match = 0;
+                                    break;
+                                }
+                            }
+                            if (match)
+                                res.push_back({{r, c}, {endRow, endCol}});
+                        }
+                    }
+                }
+            }
+        }
+        all_res.push_back(res);
+    }
+
+    return all_res;
 }
